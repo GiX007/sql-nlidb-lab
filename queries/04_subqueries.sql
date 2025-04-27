c3033737af6df6d12837a8d9e22d49201bdb4dcf
georgios_xydias <55018220+GiX007@users.noreply.github.com>
2025-04-27 09:24:03 +0300

    Update 04_subqueries.sql

--- queries/04_subqueries.sql
+++ queries/04_subqueries.sql
@@ -10,11 +10,11 @@
 --	    GROUP BY production_year) AS yearly_counts;
 -- (Subqueries allow us to prepare intermediate results inside another query — for example, first grouping data, then applying calculations like AVG or SUM on the grouped output.
 --  We usually use subqueries when we need to first aggregate, filter, or transform data and then apply further operations on the prepared intermediate results.
---  The typical thinking is: build the inner query (prepare data), then use it in the outer query to complete the final task)
+--  The typical thinking is: build the inner query (prepare data), then use it in the outer query to complete the final task.)
 
 -- 1. Find the average number of movies produced per year
 -- (First, group movies by production year and count how many were made each year by using a subquery, the inner SELECT, to prepare the grouped data.
---  Then, the outer SELECT calculates the average number of movies per year)
+--  Then, the outer SELECT calculates the average number of movies per year.)
 SELECT AVG(yearly_count) AS average_movies_per_year
 FROM (
     SELECT production_year, COUNT(*) AS yearly_count
@@ -34,16 +34,16 @@ FROM (
 ) AS movie_actor_stats;
 
 -- 3. Retrieve the latest title for video games. Display the title and its production year (check kind_type for video games)
--- (We use a subquery twice because we are applying the "only video games" filter in two different parts: once to select video games, and once to find the latest year among video games only)
+-- (We use a subquery twice because we are applying the "only video games" filter in two different parts: once to select video games, and once to find the latest year among video games only.)
 SELECT title, production_year
 FROM title
-WHERE kind_id = (	-- 1st condition: find all video games
-    SELECT id		-- (keep the titles that are video games)
+WHERE kind_id = (	-- (1st condition: find all video games.)
+    SELECT id		-- (Keep the titles that are video games.)
     FROM kind_type
     WHERE kind ILIKE '%video game%'
 )
-AND production_year = (		-- 2nd condition: find the latest production year only for video games
-    SELECT MAX(production_year) -- (pick the latest video game based on year)
+AND production_year = (		-- (2nd condition: find the latest production year only for video games.)
+    SELECT MAX(production_year) -- (Pick the latest video game based on year.)
     FROM title
     WHERE kind_id = (
         SELECT id
@@ -53,15 +53,15 @@ AND production_year = (		-- 2nd condition: find the latest production year only 
 );
 
 -- 4. List years with more than average number of movie releases
--- (First, we need to find the average, a new subquery, and then based on the results to apply the 'outer' condition)
-SELECT production_year, COUNT(*) AS movie_count	-- (3. select years with released movies above avg of 2)
+-- (First, we need to find the average, a new subquery, and then based on the results to apply the 'outer' condition.)
+SELECT production_year, COUNT(*) AS movie_count	-- (3. Select years with released movies above avg of 2.)
 FROM title
 WHERE production_year IS NOT NULL
 GROUP BY production_year
 HAVING COUNT(*) > (				
-    SELECT AVG(yearly_count)			-- (2. take the avg of results of 1 which is a number)
+    SELECT AVG(yearly_count)			-- (2. Take the avg of results of 1 which is a numbe.r)
     FROM (
-        SELECT production_year, COUNT(*) AS yearly_count	-- (1. find how many movies released in each year)
+        SELECT production_year, COUNT(*) AS yearly_count	-- (1. Find how many movies released in each year.)
         FROM title
         WHERE production_year IS NOT NULL
         GROUP BY production_year
@@ -70,17 +70,17 @@ HAVING COUNT(*) > (
 ORDER BY movie_count DESC;
 
 -- 5. Find movies with more actors than the average number of actors per movie (same as 4, but with joins)
-SELECT t.title, COUNT(*) AS actor_count	-- (3. pick the movies having more actors than the avg result number of 2. Notice we need to join all 3 tables here as we need to display titles from title table)
+SELECT t.title, COUNT(*) AS actor_count	-- (3. Pick the movies having more actors than the avg result number of 2. Notice we need to join all 3 tables here as we need to display titles from title table.)
 FROM title t
 JOIN cast_info ci ON t.id = ci.movie_id
 JOIN role_type rt ON rt.id = ci.role_id
 WHERE rt.role ILIKE '%actor%'
 GROUP BY t.title
 HAVING COUNT(*) > (
-    SELECT AVG(actor_count)		-- (2. get the avg number of actors per movie)
+    SELECT AVG(actor_count)		-- (2. Get the avg number of actors per movie.)
     FROM (
-        SELECT ci2.movie_id, COUNT(*) AS actor_count	-- (1. find how many actors each movie has. Notice that no need to join title table as movie_id is enough)
-        FROM cast_info ci2				-- (Notice the aliases became ci2, rt2 instead of ci, rt we have above)
+        SELECT ci2.movie_id, COUNT(*) AS actor_count	-- (1. Find how many actors each movie has. Notice that no need to join title table as movie_id is enough.)
+        FROM cast_info ci2				-- (Notice the aliases became ci2, rt2 instead of ci, rt we have above.)
         JOIN role_type rt2 ON rt2.id = ci2.role_id
         WHERE rt2.role ILIKE '%actor%'
         GROUP BY ci2.movie_id
@@ -88,21 +88,21 @@ HAVING COUNT(*) > (
 )
 ORDER BY actor_count DESC;
 -- (Notice that when a subquery appears inside FROM, it acts like a temporary table and we can select its columns to display, e.g. actor_count.
---  But when a subquery appears inside WHERE or HAVING, it is used only for filtering or comparing values — we cannot directly select columns from it)
+--  But when a subquery appears inside WHERE or HAVING, it is used only for filtering or comparing values — we cannot directly select columns from it.)
 
 -- (Golden Rule: Always create new aliases inside subqueries, even if they refer to the same tables as the main query.
---  Each query block must manage its own aliases independently for clarity, correctness, and future-proofing)
+--  Each query block must manage its own aliases independently for clarity, correctness, and future-proofing.)
 
 -- 6. List actors who have starred (supposing 1 < nr_order < 5) in more movies than Tom Hanks
--- (First, find how many starring roles Tom Hanks has, then find all actors who have more starring roles than him)
-SELECT n.name, COUNT(*) AS starring_roles  -- (2. get the number of starring roles for each actor and display the actor's name that verify the condition 1)
+-- (First, find how many starring roles Tom Hanks has, then find all actors who have more starring roles than him.)
+SELECT n.name, COUNT(*) AS starring_roles  -- (2. Get the number of starring roles for each actor and display the actor's name that verify the condition 1.)
 FROM cast_info ci
 JOIN name n ON n.id = ci.person_id
 JOIN role_type rt ON rt.id = ci.role_id
 WHERE rt.role ILIKE '%actor%' AND ci.nr_order BETWEEN 1 AND 4
 GROUP BY n.name
 HAVING COUNT(*) > (
-    SELECT COUNT(*)  -- (1. find all starring roles of Tom Hanks - returns a number)
+    SELECT COUNT(*)  -- (1. Find all starring roles of Tom Hanks - returns a number.)
     FROM cast_info ci2
     JOIN name n2 ON n2.id = ci2.person_id
     JOIN role_type rt2 ON rt2.id = ci2.role_id
@@ -113,7 +113,7 @@ ORDER BY starring_roles DESC;
 -- 7. List actors who have appeared in movies tagged 'horror'
 -- (This version uses a subquery: we first find all person_ids of actors who acted in horror movies, then retrieve their names from the 'name' table using WHERE IN.
 --  Note: We could also solve this without a subquery by using direct JOINs. 
---  The result would be the same, but using JOINs may improve performance in large datasets, because SQL engines can better optimize JOIN operations with indexes compared to IN + subquery)
+--  The result would be the same, but using JOINs may improve performance in large datasets, because SQL engines can better optimize JOIN operations with indexes compared to IN + subquery.)
 SELECT DISTINCT n.name
 FROM name n
 WHERE n.id IN (
@@ -152,11 +152,11 @@ LIMIT 1;
 
 -- 10. List actors who have only starred in 'comedy' movies
 -- (We use NOT EXISTS to check that there is no movie for an actor that is tagged with something other than 'comedy'.
---  If such a non-comedy movie exists for an actor, the actor is excluded; otherwise, the actor is included in the results)
+--  If such a non-comedy movie exists for an actor, the actor is excluded; otherwise, the actor is included in the results.)
 SELECT n.name
 FROM name n
 WHERE NOT EXISTS (
-    SELECT 1  -- (SELECT 1 is used because in EXISTS subqueries, SQL only checks for the existence of rows — the actual value returned does not matter. SELECT *, or SELECT any constant would behave the same)
+    SELECT 1  -- (SELECT 1 is used because in EXISTS subqueries, SQL only checks for the existence of rows — the actual value returned does not matter. SELECT *, or SELECT any constant would behave the same.)
     FROM cast_info ci
 	JOIN role_type rt ON rt.id = ci.role_id
     JOIN title t ON ci.movie_id = t.id
@@ -166,7 +166,7 @@ WHERE NOT EXISTS (
 );
 
 -- 11. Find all movies where their directors have directed more than 15 movies
--- (We first find all directors who have directed more than 15 movies using a subquery, then retrieve the movies they directed)
+-- (We first find all directors who have directed more than 15 movies using a subquery, then retrieve the movies they directed.)
 SELECT DISTINCT t.title
 FROM title t
 JOIN cast_info ci ON ci.movie_id = t.id
@@ -203,7 +203,7 @@ ORDER BY t.title;
 -- 12. Find movies that are the only production of their company
 -- (We first find companies that have produced exactly one movie — using GROUP BY company_id and HAVING COUNT(*) = 1.
 --  Then we select the movie IDs linked to those companies.
---  Finally, we select titles whose ID matches those movie IDs)
+--  Finally, we select titles whose ID matches those movie IDs.)
 SELECT t.title
 FROM title t
 WHERE t.id IN (
@@ -221,7 +221,7 @@ WHERE t.id IN (
 -- (The subquery counts how many movies are tagged with the keyword 'action'.
 --  In the main query, we GROUP BY each keyword and count how many movies it appears in.
 --  Then, in the HAVING clause, we compare each keyword's count to the 'action' keyword count.
---  Only keywords with a higher movie count than 'action' are included in the results)
+--  Only keywords with a higher movie count than 'action' are included in the results.)
 SELECT k.keyword
 FROM keyword k
 JOIN movie_keyword mk ON k.id = mk.keyword_id
@@ -234,7 +234,7 @@ HAVING COUNT(*) > (
 );
 
 -- 14. Find actors who acted both in movies and video games
--- (An alternative structure using two EXISTS subqueries to separately check the two conditions)
+-- (An alternative structure using two EXISTS subqueries to separately check the two conditions.)
 SELECT n.name
 FROM name n
 WHERE EXISTS (
@@ -247,7 +247,7 @@ WHERE EXISTS (
 )
 AND EXISTS (
     SELECT 1
-    FROM cast_info ci2		-- (Notice we use ci2, t2, rt2, kt2 to avoid any conflicts, despite SQL works with similar to the above block's aliases)
+    FROM cast_info ci2		-- (Notice we use ci2, t2, rt2, kt2 to avoid any conflicts, despite SQL works with similar to the above block's aliases.)
     JOIN title t2 ON ci2.movie_id = t2.id
     JOIN kind_type kt2 ON t2.kind_id = kt2.id
 	JOIN role_type rt2 ON rt2.id = ci2.role_id
@@ -268,10 +268,10 @@ HAVING COUNT(DISTINCT kt.kind) = 2;
 -- (Golden Rule: When checking if two different conditions are separately true (e.g., acted in both movies and video games),
 --  using multiple EXISTS subqueries is faster and more precise than using JOINs and GROUP BY.
 --  EXISTS stops as soon as it finds the first matching row (early exit), 
---  while JOINs build all possible combinations between tables, creating duplicates and inflating the result set, which slows down execution and explains why we get more results)
+--  while JOINs build all possible combinations between tables, creating duplicates and inflating the result set, which slows down execution and explains why we get more results.)
 
 -- 15. Find companies that have produced at least one 'comedy' and one 'drama'
--- (We use two EXISTS subqueries to ensure both genres are produced by the company)
+-- (We use two EXISTS subqueries to ensure both genres are produced by the company.)
 SELECT DISTINCT cn.name
 FROM company_name cn
 WHERE EXISTS (
@@ -292,10 +292,10 @@ AND EXISTS (
 );
 
 -- 16. Find the average number of distinct movie types per actor
--- (Notice that the subquery is placed in FROM, so it acts like a temporary table, allowing us to select and use its calculated columns like distinct_types in the outer query)
+-- (Notice that the subquery is placed in FROM, so it acts like a temporary table, allowing us to select and use its calculated columns like distinct_types in the outer query.)
 SELECT AVG(distinct_types) AS avg_movie_types_per_actor
 FROM (
-    SELECT n.id, COUNT(DISTINCT kt.kind) AS distinct_types	-- (COUNT(DISTINCT kt.kind) counts how many different types of movies (e.g., movie, tv series, video game) each actor has participated in)
+    SELECT n.id, COUNT(DISTINCT kt.kind) AS distinct_types	-- (COUNT(DISTINCT kt.kind) counts how many different types of movies (e.g., movie, tv series, video game) each actor has participated in.)
     FROM name n
     JOIN cast_info ci ON n.id = ci.person_id
 	JOIN role_type rt ON rt.id = ci.role_id
@@ -307,7 +307,7 @@ FROM (
 
 -- 17. Find actors who have never acted in a 'drama' movie
 -- (The subquery checks if the actor participated in any movie tagged as 'drama' and played an acting role of actor.
---  If no such 'drama' movie exists for the actor (NOT EXISTS), the actor is included in the final result)
+--  If no such 'drama' movie exists for the actor (NOT EXISTS), the actor is included in the final result.)
 SELECT n.name
 FROM name n
 WHERE NOT EXISTS (
@@ -323,7 +323,7 @@ WHERE NOT EXISTS (
 );
 
 -- (Query 17 without subquery, using GROUP BY and HAVING.
---  We join actors with their movies and keywords, group by actor and use HAVING to exclude any actor who participated in movies tagged as 'drama')
+--  We join actors with their movies and keywords, group by actor and use HAVING to exclude any actor who participated in movies tagged as 'drama'.)
 SELECT n.name
 FROM name n
 JOIN cast_info ci ON n.id = ci.person_id
@@ -337,10 +337,10 @@ HAVING SUM(CASE WHEN k.keyword ILIKE '%drama%' THEN 1 ELSE 0 END) = 0;
 -- (Notice once again: This method avoids subqueries but can be much slower on large datasets,
 --  because JOINs create a very large intermediate result, especially when actors are linked to many movies and multiple keywords.
 --  GROUP BY + HAVING must scan and group the entire joined data, even if only a few actors match.
---  Using EXISTS in the original query is much faster, because it stops searching as soon as a single 'drama' movie is found for an actor)
+--  Using EXISTS in the original query is much faster, because it stops searching as soon as a single 'drama' movie is found for an actor.)
 
 -- 18. Find the movie title and the number of actors for each movie
--- (We use a subquery inside the SELECT clause to count actors per movie)
+-- (We use a subquery inside the SELECT clause to count actors per movie.)
 SELECT t.title,
   (SELECT COUNT(*)
    FROM cast_info ci
@@ -351,7 +351,7 @@ WHERE t.production_year IS NOT NULL
 ORDER BY actor_count DESC NULLS LAST;
 
 -- 19. Find actors whose first movie was before 1980
--- (We use a correlated subquery to find each actor's earliest movie year)
+-- (We use a correlated subquery to find each actor's earliest movie year.)
 SELECT n.name
 FROM name n
 WHERE (
@@ -363,7 +363,7 @@ WHERE (
 ) < 1980;
 
 -- 20. Find the average number of distinct genres an actor has worked in
--- (We first find the number of distinct genres for each actor and then average it)
+-- (We first find the number of distinct genres for each actor and then average it.)
 SELECT AVG(distinct_genres) AS avg_genres_per_actor
 FROM (
     SELECT n.id, COUNT(DISTINCT k.keyword) AS distinct_genres
